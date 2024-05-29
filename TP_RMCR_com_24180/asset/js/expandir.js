167448a67aa78344385b47ca4e81ce413c547b50
Marce1965 <167209230+Marce1965@users.noreply.github.com>
2024-05-29 00:42:12 -0300

    Add files via upload

---
+++ TP_RMCR_com_24180/asset/js/expandir.js
@@ -0,0 +1,367 @@
+$(document).ready(function(){
+
+
+    $('#preg-1').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-1').slideDown();
+            $('#icon-mas-1').removeClass('verde');
+            $('#icon-mas-1').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-1').slideUp();
+            $('#icon-mas-1').removeClass('rojo');
+            $('#icon-mas-1').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+    
+
+
+    $('#preg-2').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-2').slideDown();
+            $('#icon-mas-2').removeClass('verde');
+            $('#icon-mas-2').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-2').slideUp();
+            $('#icon-mas-2').removeClass('rojo');
+            $('#icon-mas-2').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+    $('#preg-3').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-3').slideDown();
+            $('#icon-mas-3').removeClass('verde');
+            $('#icon-mas-3').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-3').slideUp();
+            $('#icon-mas-3').removeClass('rojo');
+            $('#icon-mas-3').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+    $('#preg-4').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-4').slideDown();
+            $('#icon-mas-4').removeClass('verde');
+            $('#icon-mas-4').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-4').slideUp();
+            $('#icon-mas-4').removeClass('rojo');
+            $('#icon-mas-4').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+    $('#preg-5').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-5').slideDown();
+            $('#icon-mas-5').removeClass('verde');
+            $('#icon-mas-5').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-5').slideUp();
+            $('#icon-mas-5').removeClass('rojo');
+            $('#icon-mas-5').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+    $('#preg-6').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-6').slideDown();
+            $('#icon-mas-6').removeClass('verde');
+            $('#icon-mas-6').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-6').slideUp();
+            $('#icon-mas-6').removeClass('rojo');
+            $('#icon-mas-6').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+    $('#preg-7').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-7').slideDown();
+            $('#icon-mas-7').removeClass('verde');
+            $('#icon-mas-7').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-7').slideUp();
+            $('#icon-mas-7').removeClass('rojo');
+            $('#icon-mas-7').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+    $('#preg-8').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-8').slideDown();
+            $('#icon-mas-8').removeClass('verde');
+            $('#icon-mas-8').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-8').slideUp();
+            $('#icon-mas-8').removeClass('rojo');
+            $('#icon-mas-8').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+    $('#preg-9').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-9').slideDown();
+            $('#icon-mas-9').removeClass('verde');
+            $('#icon-mas-9').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-9').slideUp();
+            $('#icon-mas-9').removeClass('rojo');
+            $('#icon-mas-9').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+    $('#preg-10').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-10').slideDown();
+            $('#icon-mas-10').removeClass('verde');
+            $('#icon-mas-10').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-10').slideUp();
+            $('#icon-mas-10').removeClass('rojo');
+            $('#icon-mas-10').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+
+
+    $('#preg-11').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-11').slideDown();
+            $('#icon-mas-11').removeClass('verde');
+            $('#icon-mas-11').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-11').slideUp();
+            $('#icon-mas-11').removeClass('rojo');
+            $('#icon-mas-11').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+    $('#preg-12').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-12').slideDown();
+            $('#icon-mas-12').removeClass('verde');
+            $('#icon-mas-12').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-12').slideUp();
+            $('#icon-mas-12').removeClass('rojo');
+            $('#icon-mas-12').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+
+
+    $('#preg-13').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-13').slideDown();
+            $('#icon-mas-13').removeClass('verde');
+            $('#icon-mas-13').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-13').slideUp();
+            $('#icon-mas-13').removeClass('rojo');
+            $('#icon-mas-13').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+
+
+
+    $('#preg-14').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-14').slideDown();
+            $('#icon-mas-14').removeClass('verde');
+            $('#icon-mas-14').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-14').slideUp();
+            $('#icon-mas-14').removeClass('rojo');
+            $('#icon-mas-14').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+
+
+
+    $('#preg-15').toggle( 
+  
+        // Primer click
+        function(e){ 
+            $('#resp-15').slideDown();
+            $('#icon-mas-15').removeClass('verde');
+            $('#icon-mas-15').html('<span class="fas fa-minus"></span>').addClass('rojo');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+      
+        // Segundo click
+        function(e){ 
+            $('#resp-15').slideUp();
+            $('#icon-mas-15').removeClass('rojo');
+            $('#icon-mas-15').html('<span class="fas fa-plus"></span>').addClass('verde');
+            e.preventDefault();
+        }
+  
+    );
+
+
+
+
+
+
+
+
+});
+
